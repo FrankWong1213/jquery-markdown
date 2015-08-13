7e791b6adb713aee05208023d80ba70761b39885
frankwong1213 <frankwong1213@163.com>
2015-08-13 11:53:03 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# jquery-markdown
 a demo for displaying markdown on html
+
+refer to:https://github.com/taknakamu/jquery-markdown
+
+http://25.io/mou/
+
---
+++ javascripts/demo.js
@@ -0,0 +1,19 @@
+var $validator;
+
+jQuery(document).ready(function(){
+
+    $(".markdown-text > textarea").markdown({
+        target_form   : ".markdown"
+    });
+
+    $(".markdown-text > textarea").on("keyup", function() {
+        $(this).markdown({
+            target_form   : ".markdown"
+        });
+    });
+
+    $(".close").on("click", function() {
+        $(this).closest('footer').fadeOut();
+    });
+
+});
